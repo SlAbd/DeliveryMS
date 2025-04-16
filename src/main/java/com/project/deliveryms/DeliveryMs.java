4f614f4f83f47edcc636a0be3c7e0851d97bd3db
Abdelhadi Slimane <sliwabdel15@gmail.com>
2025-04-16 14:58:17 +0100

    "projet"

---
+++ src/main/java/com/project/deliveryms/DeliveryMs.java
@@ -0,0 +1,13 @@
+package com.project.deliveryms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMs {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryMs.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/deliveryms/DeliveryMsTests.java
@@ -0,0 +1,13 @@
+package com.project.deliveryms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMsTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
